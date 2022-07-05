5803f1a1f92157d3bed2d799ae49a606580f2fd9
shiba2046@work <17551496+shiba2046@users.noreply.github.com>
2022-07-05 09:30:04 +0800

    Removed local file require

--- lawschool-training-tool.js
+++ lawschool-training-tool.js
@@ -9,7 +9,8 @@
 // @icon         https://lawschool.lawyerpass.com/assets/images/favicon.ico
 // @downloadURL  https://raw.githubusercontent.com/shiba2046/greasemonkey-scripts/main/lawschool-training-tool.js
 // @updateURL    https://raw.githubusercontent.com/shiba2046/greasemonkey-scripts/main/lawschool-training-tool.js
-// @require      file:///home/peng/Dev/greasemonkey-scripts/lawschool-training-tool.js
+
+///// @require      file:///home/peng/Dev/greasemonkey-scripts/lawschool-training-tool.js
 // @grant        none
 // @license      MIT
 // ==/UserScript==
